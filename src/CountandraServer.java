8057af212222b78d12e838dfd4d3cf223f8d1a9c
= <nehal413@gmail.com>
2011-12-28 05:29:32 +0530

    Initialize CassandraDB to cassandrahostip

--- src/CountandraServer.java
+++ src/CountandraServer.java
@@ -1,23 +1,22 @@
 import java.io.*;
 import org.apache.cassandra.thrift.CassandraServer;
-import org.apache.cassandra.thrift.CfDef; 
-import org.apache.cassandra.thrift.Column; 
-import org.apache.cassandra.thrift.ColumnOrSuperColumn; 
-import org.apache.cassandra.thrift.ColumnParent; 
-import org.apache.cassandra.thrift.ColumnPath; 
-import org.apache.cassandra.thrift.ConsistencyLevel; 
-import org.apache.cassandra.thrift.KsDef; 
-import org.apache.cassandra.thrift.Mutation; 
-import org.apache.cassandra.thrift.SlicePredicate; 
-import org.apache.cassandra.thrift.SliceRange; 
-import org.apache.cassandra.utils.ByteBufferUtil; 
+import org.apache.cassandra.thrift.CfDef;
+import org.apache.cassandra.thrift.Column;
+import org.apache.cassandra.thrift.ColumnOrSuperColumn;
+import org.apache.cassandra.thrift.ColumnParent;
+import org.apache.cassandra.thrift.ColumnPath;
+import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.apache.cassandra.thrift.KsDef;
+import org.apache.cassandra.thrift.Mutation;
+import org.apache.cassandra.thrift.SlicePredicate;
+import org.apache.cassandra.thrift.SliceRange;
+import org.apache.cassandra.utils.ByteBufferUtil;
 
-import java.nio.ByteBuffer; 
-import java.util.ArrayList; 
-import java.util.HashMap; 
-import java.util.List; 
-import java.util.Map; 
-
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.PosixParser;
@@ -29,7 +28,6 @@ import org.countandra.netty.*;
 import org.countandra.cassandra.*;
 import org.countandra.utils.*;
 
-
 // CassandraServer is local (with CassandraYaml specify the data store and distributed servers)  
 //    -local-cassandra -local-cassandra-thrift-port -local-cassandra-thrift-ip
 //      Therefore start the CassandraServer and
@@ -40,118 +38,128 @@ import org.countandra.utils.*;
 //      just need a pointer there to the host and port (could be localhost and default port)
 //      -remote-cassandra -remote-cassandra-thrift-port -remote-cassandra-thrift-ip
 
-
 // HttpServer is always assumed to be local and should  be started 
 // --http-server -http-server-port
 
-     
-
 public class CountandraServer {
-    
-    public static Options options = new Options(); 
-    
-    static {
-	options.addOption("s", "server-mode", false, " Cassandra Server Mode");
-	options.addOption("i", "init", false, " Initialize Cassandra with basic structures");
-	options.addOption("h", "httpserver", false, " Whether to include httserver");	
-	options.addOption("httpserverport", "httpserverport", true, " httpserver port in case the default of 8080 does not work");
-	options.addOption("cassandrahostip", "cassandrahostip", true, " cassandra host ip for  httpserverto communicate to");
-	options.addOption("consistencylevel", "consistencylevel", true, " consistency levels of writes/reads");
-	
-	options.addOption("replicationfactor", "replicationfactor", true, " replicas of data");
-	options.addOption("locatorstrategy", "locatorstrategy", true, "how the rows should be located");
 
-	
+	public static Options options = new Options();
 
-    }
+	static {
+		options.addOption("s", "server-mode", false, " Cassandra Server Mode");
+		options.addOption("i", "init", false,
+				" Initialize Cassandra with basic structures");
+		options.addOption("h", "httpserver", false,
+				" Whether to include httserver");
+		options.addOption("httpserverport", "httpserverport", true,
+				" httpserver port in case the default of 8080 does not work");
+		options.addOption("cassandrahostip", "cassandrahostip", true,
+				" cassandra host ip for  httpserverto communicate to");
+		options.addOption("consistencylevel", "consistencylevel", true,
+				" consistency levels of writes/reads");
 
-    private static int httpPort = 8080;
-    private static String cassandraServerForClient = new String("localhost:9160");
-    
-   
-    public static void main (String args[])  {
+		options.addOption("replicationfactor", "replicationfactor", true,
+				" replicas of data");
+		options.addOption("locatorstrategy", "locatorstrategy", true,
+				"how the rows should be located");
 
-	try {
-	    System.out.println(args[0]);
-
-	    CommandLineParser parser = new PosixParser();
-	    CommandLine line = parser.parse( options, args );
-
-	    if (line.hasOption("cassandrahostip")) {
-		if (line.hasOption("consistencylevel")) {
-		    if (line.hasOption("replicationfactor")) {
-
-			    CassandraStorage.setGlobalParams(line.getOptionValue("cassandrahostip"), line.getOptionValue("consistencylevel"));
-			    CassandraDB.setGlobalParams(line.getOptionValue("cassandrahostip"), line.getOptionValue("replicationfactor"));
-
-		    }
-		    else {
-
-			    CassandraStorage.setGlobalParams(line.getOptionValue("cassandrahostip"), line.getOptionValue("consistencylevel"));
-			    CassandraDB.setGlobalParams(line.getOptionValue("cassandrahostip"));
-
-		    }
-		}
-
-		else {  // no consistency level -- assumed to be ONE
-		    if (line.hasOption("replicationfactor")) {
-
-			    CassandraStorage.setGlobalParams(line.getOptionValue("cassandrahostip"));
-			    CassandraDB.setGlobalParams(line.getOptionValue("cassandrahostip"), line.getOptionValue("replicationfactor"));
-
-		    }
-		    else {
-
-			    CassandraStorage.setGlobalParams(line.getOptionValue("cassandrahostip"));
-			    CassandraDB.setGlobalParams(line.getOptionValue("cassandrahostip"));
-
-		    }
-		}
-	    }
-
- 	    if (line.hasOption("s")) {
-		System.out.println("Starting Cassandra");
-		// cassandra server
-		CassandraUtils.startupCassandraServer();
-	    }
-	    if (line.hasOption("i")) {
-	     if (line.hasOption("cassandrahostip") ) {
-		System.out.println("Initializing Basic structures");
-		CountandraUtils.initBasicDataStructures(line.getOptionValue("cassandrahostIp"));
-		System.out.println("Initialized Basic structures");
-             }
-             else{
-		System.out.println("Initializing Basic structures");
-		CountandraUtils.initBasicDataStructures();
-		System.out.println("Initialized Basic structures");
-             }
-	    }
-	    
-	    if (line.hasOption("h")) {
-		
-		if (line.hasOption("httpserverport") ) {
-		    httpPort = Integer.parseInt(line.getOptionValue("httpserverport"));
-		}
-
-		if (line.hasOption("cassandrahostip") ) {
-		    CountandraUtils.setCassandraHostIp(line.getOptionValue("cassandrahostIp"));
-		}
-		else {
-		    CountandraUtils.setCassandraHostIp("localhost:9160");
-		}
-
-		    
-		NettyUtils.startupNettyServer(httpPort); 
-	    }
-	    // http server
-
-	
-	} 
-	catch (IOException ioe) {
-	    System.out.println(ioe);
 	}
-	catch (Exception e) {
-	    System.out.println(e);
+
+	private static int httpPort = 8080;
+	private static String cassandraServerForClient = new String(
+			"localhost:9160");
+
+	public static void main(String args[]) {
+
+		try {
+			System.out.println(args[0]);
+
+			CommandLineParser parser = new PosixParser();
+			CommandLine line = parser.parse(options, args);
+
+			if (line.hasOption("cassandrahostip")) {
+				if (line.hasOption("consistencylevel")) {
+					if (line.hasOption("replicationfactor")) {
+
+						CassandraStorage.setGlobalParams(
+								line.getOptionValue("cassandrahostip"),
+								line.getOptionValue("consistencylevel"));
+						CassandraDB.setGlobalParams(
+								line.getOptionValue("cassandrahostip"),
+								line.getOptionValue("replicationfactor"));
+
+					} else {
+
+						CassandraStorage.setGlobalParams(
+								line.getOptionValue("cassandrahostip"),
+								line.getOptionValue("consistencylevel"));
+						CassandraDB.setGlobalParams(line
+								.getOptionValue("cassandrahostip"));
+
+					}
+				}
+
+				else { // no consistency level -- assumed to be ONE
+					if (line.hasOption("replicationfactor")) {
+
+						CassandraStorage.setGlobalParams(line
+								.getOptionValue("cassandrahostip"));
+						CassandraDB.setGlobalParams(
+								line.getOptionValue("cassandrahostip"),
+								line.getOptionValue("replicationfactor"));
+
+					} else {
+
+						CassandraStorage.setGlobalParams(line
+								.getOptionValue("cassandrahostip"));
+						CassandraDB.setGlobalParams(line
+								.getOptionValue("cassandrahostip"));
+
+					}
+				}
+			}
+
+			if (line.hasOption("s")) {
+				System.out.println("Starting Cassandra");
+				// cassandra server
+				CassandraUtils.startupCassandraServer();
+			}
+			if (line.hasOption("i")) {
+				if (line.hasOption("cassandrahostip")) {
+					System.out.println("Initializing Basic structures");
+					CountandraUtils.setCassandraHostIp(line
+							.getOptionValue("cassandrahostIp"));
+					CountandraUtils.initBasicDataStructures();
+					System.out.println("Initialized Basic structures");
+				} else {
+					System.out.println("Initializing Basic structures");
+					CountandraUtils.initBasicDataStructures();
+					System.out.println("Initialized Basic structures");
+				}
+			}
+
+			if (line.hasOption("h")) {
+
+				if (line.hasOption("httpserverport")) {
+					httpPort = Integer.parseInt(line
+							.getOptionValue("httpserverport"));
+				}
+
+				if (line.hasOption("cassandrahostip")) {
+					CountandraUtils.setCassandraHostIp(line
+							.getOptionValue("cassandrahostIp"));
+				} else {
+					CountandraUtils.setCassandraHostIp("localhost:9160");
+				}
+
+				NettyUtils.startupNettyServer(httpPort);
+			}
+			// http server
+
+		} catch (IOException ioe) {
+			System.out.println(ioe);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-    }
 }
--- src/org/countandra/cassandra/CassandraDB.java
+++ src/org/countandra/cassandra/CassandraDB.java
@@ -1,30 +1,29 @@
 package org.countandra.cassandra;
 
-import java.nio.ByteBuffer; 
-import java.util.ArrayList; 
-import java.util.HashMap; 
-import java.util.List; 
-import java.util.Map; 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-
-import org.apache.cassandra.thrift.CassandraServer; 
+import org.apache.cassandra.thrift.CassandraServer;
 import org.apache.cassandra.thrift.Cassandra.*;
 import org.apache.cassandra.thrift.Cassandra;
 
-import org.apache.cassandra.thrift.CfDef; 
-import org.apache.cassandra.thrift.Column; 
-import org.apache.cassandra.thrift.ColumnOrSuperColumn; 
-import org.apache.cassandra.thrift.ColumnParent; 
+import org.apache.cassandra.thrift.CfDef;
+import org.apache.cassandra.thrift.Column;
+import org.apache.cassandra.thrift.ColumnOrSuperColumn;
+import org.apache.cassandra.thrift.ColumnParent;
 import org.apache.cassandra.thrift.CounterColumn;
-import org.apache.cassandra.thrift.ColumnPath; 
-import org.apache.cassandra.thrift.ConsistencyLevel; 
-import org.apache.cassandra.thrift.KsDef; 
-import org.apache.cassandra.thrift.Mutation; 
-import org.apache.cassandra.thrift.SlicePredicate; 
-import org.apache.cassandra.thrift.SliceRange; 
-import org.apache.cassandra.utils.ByteBufferUtil; 
-import org.json.simple.JSONObject; 
-import org.json.simple.JSONValue; 
+import org.apache.cassandra.thrift.ColumnPath;
+import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.apache.cassandra.thrift.KsDef;
+import org.apache.cassandra.thrift.Mutation;
+import org.apache.cassandra.thrift.SlicePredicate;
+import org.apache.cassandra.thrift.SliceRange;
+import org.apache.cassandra.utils.ByteBufferUtil;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSocket;
@@ -32,106 +31,97 @@ import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.TException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class CassandraDB {
+	static public CassandraServer server = new CassandraServer();
+	private static Logger log = LoggerFactory.getLogger(CassandraDB.class);
+	/*
+	 * Set's thread local value on the server.
+	 */
 
-import org.slf4j.Logger;import org.slf4j.LoggerFactory;
+	static private Cassandra.Client client = null;
+	static TTransport tr = null;
+	static TProtocol proto = null;
 
+	static String cassandraHost = new String("localhost");
+	static int cassandraPort = 9160;
 
+	static {
+		/*
+		 * tr = new TFramedTransport(new TSocket(cassandraHost, cassandraPort));
+		 * proto = new TBinaryProtocol(tr); client = new
+		 * Cassandra.Client(proto);
+		 */
 
-public class CassandraDB
-{ 
-    static public CassandraServer server = new CassandraServer(); 
-    private static Logger log = LoggerFactory.getLogger(CassandraDB.class); 
-    /* 
-     * Set's thread local value on the server. 
-     */ 
+	}
 
-    static private Cassandra.Client client = null;
-    static TTransport tr = null;
-    static TProtocol proto = null;
+	private static String s_locatorStrategy = new String(
+			"org.apache.cassandra.locator.SimpleStrategy");
+	private static String s_replicationFactor = new String("1");
 
+	public static synchronized void setGlobalParams(String hostIp) {
+		String[] split_hostIp = hostIp.split(":");
+		cassandraHost = split_hostIp[0];
+		cassandraPort = Integer.parseInt(split_hostIp[1]);
+	}
 
-    static String cassandraHost = new String("localhost");
-    static int cassandraPort = 9160;
-     
-    static {
-	/*	
-	tr = new TFramedTransport(new TSocket(cassandraHost, cassandraPort));
-	proto = new TBinaryProtocol(tr);
-	client = new Cassandra.Client(proto);
-	*/
-	
-    }
-    
-    
-    
+	public static synchronized void setGlobalParams(String hostIp,
+			String replicationFactor) {
+		setGlobalParams(hostIp);
+		s_replicationFactor = replicationFactor;
+	}
 
-    private static String s_locatorStrategy = new String ("org.apache.cassandra.locator.SimpleStrategy");
-    private static String s_replicationFactor = new String("1");
-    
-    public static synchronized void setGlobalParams(String hostIp) {
-	String [] split_hostIp = hostIp.split(":");
-	cassandraHost = split_hostIp[0];
-	cassandraPort = Integer.parseInt(split_hostIp[1]);
-    }
-    public static synchronized void setGlobalParams(String hostIp, String replicationFactor) {
-	setGlobalParams( hostIp);
-	s_replicationFactor = replicationFactor;
-    }
+	public void addKeyspace(String keyspace) throws Exception {
 
+		addKeyspace(keyspace, s_locatorStrategy, s_replicationFactor);
 
+	}
 
-    public void addKeyspace(String keyspace) throws Exception 
-    { 
-	addKeyspace(keyspace, s_locatorStrategy, s_replicationFactor);
-	
-    } 
+	public void addKeyspace(String keyspace, String locatorStrategy,
+			String replicationFactor) throws Exception {
 
+		List<CfDef> cfDefList = new ArrayList<CfDef>();
+		KsDef ksDef = new KsDef(keyspace, locatorStrategy, cfDefList);
+		ksDef.putToStrategy_options("replication_factor", replicationFactor);
 
-    public void addKeyspace(String keyspace, String locatorStrategy, String replicationFactor) throws Exception  {
-	
-	List<CfDef> cfDefList = new ArrayList<CfDef>(); 
-	KsDef ksDef = new KsDef(keyspace, locatorStrategy, cfDefList); 
-	ksDef.putToStrategy_options("replication_factor", replicationFactor); 
+		tr = new TFramedTransport(new TSocket(cassandraHost, cassandraPort));
+		proto = new TBinaryProtocol(tr);
+		client = new Cassandra.Client(proto);
 
- 	tr = new TFramedTransport(new TSocket(cassandraHost, cassandraPort));
-	proto = new TBinaryProtocol(tr);
-	client = new Cassandra.Client(proto);
+		tr.open();
 
-	tr.open();
-	
-	client.system_add_keyspace(ksDef); 
+		client.system_add_keyspace(ksDef);
 
-	tr.close();
+		tr.close();
 
+	}
 
-    }
-    
-   
-    public void createColumnFamily(String keyspace, String columnFamilyName) throws Exception 
-    { 
-	this.createColumnFamily(keyspace, columnFamilyName, "UTF8Type", "UTF8Type", "UTF8Type");
-    } 
+	public void createColumnFamily(String keyspace, String columnFamilyName)
+			throws Exception {
+		this.createColumnFamily(keyspace, columnFamilyName, "UTF8Type",
+				"UTF8Type", "UTF8Type");
+	}
 
-    public void createColumnFamily(String keyspace, String columnFamilyName, String keyValidationClass, String comparatorType,String defaultValidationClass) 
-	throws Exception 
-    { 
-	CfDef columnFamily = new CfDef(keyspace, columnFamilyName); 
-	columnFamily.setKey_validation_class(keyValidationClass); 
-	columnFamily.setComparator_type(comparatorType); 
-	columnFamily.setDefault_validation_class(defaultValidationClass); 
+	public void createColumnFamily(String keyspace, String columnFamilyName,
+			String keyValidationClass, String comparatorType,
+			String defaultValidationClass) throws Exception {
 
+		CfDef columnFamily = new CfDef(keyspace, columnFamilyName);
+		columnFamily.setKey_validation_class(keyValidationClass);
+		columnFamily.setComparator_type(comparatorType);
+		columnFamily.setDefault_validation_class(defaultValidationClass);
 
-	tr = new TFramedTransport(new TSocket(cassandraHost, cassandraPort));
-	proto = new TBinaryProtocol(tr);
-	client = new Cassandra.Client(proto);
+		tr = new TFramedTransport(new TSocket(cassandraHost, cassandraPort));
+		proto = new TBinaryProtocol(tr);
+		client = new Cassandra.Client(proto);
 
-	tr.open();
-	client.set_keyspace(keyspace); 
-	client.system_add_column_family(columnFamily); 
-	tr.close();
-	
-    } 
+		tr.open();
+		client.set_keyspace(keyspace);
+		client.system_add_column_family(columnFamily);
+		tr.close();
 
+	}
 
 }
